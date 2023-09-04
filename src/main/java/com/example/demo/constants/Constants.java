551f27f9bde8fc5785fe37e4f6590bfa40551472
Bui Vu Thien An <anbvt562003@gmail.com>
2023-09-05 02:19:35 +0700

    Final Source

--- src/main/java/com/example/demo/constants/Constants.java
+++ src/main/java/com/example/demo/constants/Constants.java
@@ -1,6 +1,6 @@
 package com.example.demo.constants;
 
 public class Constants {
-    public static final String REGISTRATION = "/util/SendOTP";
+    public static final String URL = "http://localhost:8080";
     public static final int TIMETOKEN_ACTIVE = 1000 * 60 * 5; // 5 Minute
 }
